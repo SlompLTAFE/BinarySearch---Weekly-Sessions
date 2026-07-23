5893492be8ee344f64d8da2a1e67a59276b6def5
SlompLTAFE <20069199@tafe.wa.edu.au>
2026-07-23 14:34:37 +0800

    Functioning Version of Binary Search Algorithm, included left and right to manage the index of the array.

--- binary_search.py
+++ binary_search.py
@@ -1,9 +1,19 @@
 array_in_question = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-
-def find(array, input):
+def searcharray(array, input):
     if input not in array:
         raise ValueError("value not in array")
     
-    index = len(array) / 2
-    pass
+    left, right = 0, len(array) - 1
+    while left <= right:
+        middle = (left + right) // 2 # uses both backslashes to get the lower rounded value of the division
+        if array[middle] == input:
+            return middle
+        elif array[middle] < input:
+            left = middle + 1
+        elif array[middle] > input:
+            right = middle - 1
+        else:
+            return -1
+
+found_index = searcharray(array_in_question, 1)
