6aea2546468fb7579bcca2ad5720b71b8d209845
SlompLTAFE <20069199@tafe.wa.edu.au>
2026-09-18 09:24:23 +0800

    Final Update, had issues with vscode but ran through the test.

--- binary_search.py
+++ binary_search.py
@@ -1,26 +1,19 @@
-array_in_question = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] #can comment this out and use an array of your choice.
+array_in_question = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
-
-def searcharray(array, input):
+def find(array, input):
     if input not in array:
         raise ValueError("value not in array")
-    
+
     left, right = 0, len(array) - 1
+
     while left <= right:
-        middle = (left + right) // 2 # uses both backslashes to get the lower rounded value of the division
+        middle = (left + right) // 2
+
         if array[middle] == input:
             return middle
         elif array[middle] < input:
             left = middle + 1
         elif array[middle] > input:
             right = middle - 1
-
-user_input = input("put in a number to search for in the array: 1-10: ")
-
-found_index = searcharray(array_in_question, int(user_input)) #Simply calls the fucntion and passes the array and input value we want to find.
-print (f"Value found at index:{found_index}  contains = {array_in_question[found_index]}")
-
-input("Press enter to exit the program")
-
 
